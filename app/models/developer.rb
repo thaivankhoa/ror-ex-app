543fa15804139a09127c71a06bfd55d5ee0d514a
thaivankhoa <thaivankhoa2802@gmail.com>
2018-09-07 11:32:42 +0700

    add ralation between

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,2 +1,7 @@
 class Developer < ApplicationRecord
+  has_many :developer_languages
+  has_many :languages, through: :developer_languages
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 end
--- app/models/language.rb
+++ app/models/language.rb
@@ -1,2 +1,6 @@
 class Language < ApplicationRecord
+  has_many :developer_languages
+  has_many :developers, through: :developer_languages
+
+  validates :code, presence: true
 end
